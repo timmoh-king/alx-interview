8b2757587a7f30699488b3d1b1aaa2549a453bfd
timmoh-king <timothykaruki43@gmail.com>
2023-09-28 10:17:38 +0300

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,15 +10,12 @@
 
 def pascal_triangle(n):
     """initialize an empty list"""
-    triangle = []
+    triangle = [[1]]
 
-    for i in range(n):
-        """initialize each row with 1s"""
-        row = [1] * (i + 1)
-
-        """calculate values btn first and last element"""
-        for j in range(1, i):
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-            triangle.append(row)
-
+    for i in range(n - 1):
+        temp = [0] + triangle[-1] + [0]
+        row = []
+        for j in range(len(triangle[-1]) + 1):
+            row.append(temp[j] + temp[j + 1])
+        triangle.append(row)
     return triangle
