b269ebd27c6477ee49d7dcff322862c3aa419937
timmoh-king <timothykariuki43@gmail.com>
2023-11-22 16:57:21 +0300

    0-starwars_characters.js

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+/**
+ * Write a script that prints all characters of a Star Wars movie:
+ */
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+const get = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body));
+      }
+    });
+  });
+};
+
+get(`https://swapi-api.alx-tools.com/api/films/${movieId}/`)
+  .then(response => Promise.all(response.characters.map(url => get(url))))
+  .then(responses => responses.map(response => response.name))
+  .then(names => names.forEach(name => console.log(name)))
+  .catch(error => console.error(error));
