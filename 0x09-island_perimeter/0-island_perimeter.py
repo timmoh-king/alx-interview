848ae43bd8694230a5c19bf820bdaa31e6fb9114
timmoh-king <timothykariuki43@gmail.com>
2023-11-30 09:32:31 +0300

    0x09-island_perimeter directory

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+    Create a function def island_perimeter(grid):
+"""
+
+
+def island_perimeter(grid):
+    """
+        returns the perimeter of the island described in grid:
+    """
+    pass
